d51b7923ce2bd6452fb0dcdb893feab4a39fac21
Mark Scala <markscala@gmail.com>
2014-09-25 17:17:02 -0500

    latest

--- oo-ttt.rb
+++ oo-ttt.rb
@@ -22,7 +22,7 @@ class Board
   end
 
   def place_mark(cell, mark)
-    if cell < 1 || 9 < cell || not mark =~ /X|O/
+    if cell < 1 || 9 < cell || mark != 'X' || mark != 'O'
       return "Illegal Cell"
     else
       self.board[self.cell_map[cell]] = mark
@@ -30,7 +30,16 @@ class Board
   end
 
   def clear_board
-    self.cell_map.keys.each { |k| self.board[k] = " " }
+    self.cell_map.values.each { |v| self.board[v] = " " }
+  end
+
+  def open_cells
+    self.cell_map.values.map { |k,v | k if self.board[v] == ' ' }
+  end
+
+  def closed_cells
+    (1..9).to_a - closed_cells()
+  end
 
   def to_s
     return self.board
@@ -38,11 +47,12 @@ class Board
 end
 
 class Player
-  attr_accessor :name
+  attr_accessor :name, :moves
 
-  def intialize(name, mark='X')
+  def initialize(name, mark='X')
     @name = name
     @mark = mark
+    @moves = []
   end
 
   def to_s
@@ -51,11 +61,19 @@ class Player
 end
 
 class HumanPlayer < Player
-  def intialize(name, mark='X')
+  def initialize(name, mark='X')
     super(name, mark)
   end
 
   def choose
+    puts "Choose a cell: (1-9)"
+    choice = gets.chomp.to_i
+    while choice < 1 && choice > 9
+      puts "Eh? Choose a cell: (1-9)"
+      choice = gets.chomp.to_i
+    end
+    self.moves << choice
+    choice
   end
 end
 
@@ -64,22 +82,39 @@ class BotPlayer < Player
     super(name, mark)
   end
 
-  def choose
+  def choose(open_cells)
+    choice = open_cells.sample
+    self.moves << choice
+    choice
   end
 end
 
 class Game
   attr_accessor :human, :bot, :board
 
-  def intialize(human, bot, board)
+  def initialize(human, bot, board)
     @human = human
     @bot = bot
     @board = board
   end
 
+  def winner?(player)
+    winners =
+      [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [3,5,7], [1,5,9]]
+    winners.each do |w|
+      if w - player.moves == []
+        return true
+      end
+    end
+  end
+
   def run
     while true
+      puts self.board
       break
     end
   end
 end
+
+g = Game.new(HumanPlayer.new("Mark", "X"), BotPlayer.new("Cruncher", "O"), Board.new())
+g.run()
