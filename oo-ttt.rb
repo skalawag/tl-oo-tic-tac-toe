b0e34ad92b0b2aa80a815e0c5333e9f9b439f24e
Mark Scala <markscala@gmail.com>
2014-09-25 18:40:10 -0500

    working state (niceties aside)

--- oo-ttt.rb
+++ oo-ttt.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Board
   attr_accessor :cells, :board
   attr_reader :cell_map
@@ -22,7 +24,7 @@ class Board
   end
 
   def place_mark(cell, mark)
-    if cell < 1 || 9 < cell || mark != 'X' || mark != 'O'
+    if cell < 1 || 9 < cell
       return "Illegal Cell"
     else
       self.board[self.cell_map[cell]] = mark
@@ -34,7 +36,7 @@ class Board
   end
 
   def open_cells
-    self.cell_map.values.map { |k,v | k if self.board[v] == ' ' }
+    self.cell_map.map { |k,v | k if self.board[v] == ' ' }.compact
   end
 
   def closed_cells
@@ -47,7 +49,7 @@ class Board
 end
 
 class Player
-  attr_accessor :name, :moves
+  attr_accessor :name, :moves, :mark
 
   def initialize(name, mark='X')
     @name = name
@@ -61,6 +63,7 @@ class Player
 end
 
 class HumanPlayer < Player
+
   def initialize(name, mark='X')
     super(name, mark)
   end
@@ -98,23 +101,88 @@ class Game
     @board = board
   end
 
-  def winner?(player)
+  def winner?(player=nil)
     winners =
       [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [3,5,7], [1,5,9]]
     winners.each do |w|
-      if w - player.moves == []
-        return true
+      if player.nil?
+        test1 = w - self.human.moves
+        test2 = w - self.bot.moves
+        if test1.empty? || test2.empty?
+          return true
+        end
+      else
+        test = w - player.moves
+        if test.empty?
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def run_player(player)
+    if self.board.open_cells.empty?
+      return 1
+    else
+      if player.name == self.bot.name
+        self.board.place_mark(player.choose(self.board.open_cells()), player.mark)
+      else
+        self.board.place_mark(player.choose(), player.mark)
+      end
+      if winner?()
+        return 1
       end
     end
   end
 
-  def run
+  def reset
+    self.board.clear_board
+    self.human.moves = []
+    self.bot.moves = []
+  end
+
+  def display_board
+    system 'clear'
+    puts self.board
+  end
+
+  def run_game
+    rand(2) == 0 ? first = self.human : first = self.bot
+    first == self.human ? second = self.bot : second = self.human
+
     while true
-      puts self.board
-      break
+      # inner loop handles one game
+      while true
+        display_board()
+        if run_player(first) == 1
+            break
+        end
+        if run_player(second) == 1
+            break
+        end
+      end
+      # catch the last move
+      display_board()
+
+      if winner?(self.human)
+        puts "#{human.name} has won!}"
+      elsif winner?(self.bot)
+        puts "#{bot.name} has won!}"
+      else
+        puts "Tie!"
+      end
+
+      puts "Would you like to play another? (y/n)"
+      if gets.chomp != 'y'
+        break
+      else
+        reset()
+      end
     end
   end
 end
 
-g = Game.new(HumanPlayer.new("Mark", "X"), BotPlayer.new("Cruncher", "O"), Board.new())
-g.run()
+Game.new(HumanPlayer.new("Mark", "X"),
+         BotPlayer.new("Bot", "O"),
+         Board.new()).run_game
